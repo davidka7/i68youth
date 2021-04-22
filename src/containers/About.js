402733508fbed26ee07bc4f35e1ffa07cd843c0e
davidka7 <david.perekopskiy@go.shoreline.edu>
2021-04-22 14:37:29 -0700

    s

--- src/containers/About.js
+++ src/containers/About.js
@@ -9,13 +9,26 @@ function About() {
       <br />
       <div className="flex">
         <div className="left">
-          Life Is good at i68 Youth
-          <br />
-          We serve and do our best!
-          <br />
-          We Love God with all our hearts,
-          <br />
-          and find the truth in the scripture!
+          i68 is the youth ministry of Church of the Living God. Our goal and
+          earnest desire is to see thousands of young people transformed by our
+          ministry. We want to help equip young people with the right mentality
+          to be able to overcome the pressures of their flesh, and the constant
+          pressures of this dark world. God wants to cleanse us and make us into
+          a holy nation and we put a strong emphasis on working on ourselves so
+          that we can be those pure vessels that God can use. We know that this
+          is a difficult and hard process but we willingly choose to take part
+          in this long operation. Our characters are changed, habits are broken,
+          chains are breached, and hearts are healed. We believe that God has
+          called us to be modern-day John the Baptists. We refuse to compromise
+          the Word of God and make it something that it is not. We will not take
+          the easy road. We firmly believe in reading God’s Word everyday and
+          letting it transform the way that we think. We pray, both individually
+          and corporately, and believe that we will see revival as a result of
+          our prayers! We are constantly convicted by God’s Word and this
+          conviction won’t allow us to live a quiet and selfish life. We choose
+          to put the Word into action by preaching the Gospel wherever we
+          possibly can. That is the goal every single one of us is striving
+          towards!
         </div>{" "}
         <img src={family} className="familylogo"></img>
       </div>
